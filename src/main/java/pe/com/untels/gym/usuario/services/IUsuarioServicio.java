4f189201757bf939c3276a9049e296a08c6b99e3
Joel Barrios <2223010245@untels.edu.pe>
2026-07-04 20:14:13 -0500

    Insert implementado

--- src/main/java/pe/com/untels/gym/usuario/services/IUsuarioServicio.java
+++ src/main/java/pe/com/untels/gym/usuario/services/IUsuarioServicio.java
@@ -1,9 +1,12 @@
 package pe.com.untels.gym.usuario.services;
 
+import pe.com.untels.gym.usuario.dtos.UsuarioDatosDTO;
 import pe.com.untels.gym.usuario.dtos.UsuarioResponseDTO;
 
 import java.util.List;
 
 public interface IUsuarioServicio {
     List<UsuarioResponseDTO> listarUsuarios();
+    UsuarioDatosDTO perfil(Integer id);
 }
+
